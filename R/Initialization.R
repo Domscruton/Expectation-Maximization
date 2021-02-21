5b8859bf6d87c7e9f8ae4da77a61ba0f240471ce
Dom <dom.scruton@hotmail.com>
2021-02-21 14:33:42 +0000

    Finished random_initialization and added methods to initialization_fun

--- R/Initialization.R
+++ R/Initialization.R
@@ -3,28 +3,33 @@
 
 # Random Assignment -------------------------------------------------------
 
-random_initialization <- function(X, target, K, clusters){
+random_initialization <- function(X, target, K, clusters, p){
   # Inputs:
     # x: dataframe containing features
     # y: character string identify the column
     # K: no. clusters
     # clusters: cluster values to impute
+    # p: vector of probability weights for each cluster
   # calculate a vector of probability weights
   # for the elements that are NA, we want to sample- remember that we want to
   # keep the target values for those that already exist.
   for (i in which(is.na(target))) {
-    target[i] <- sample(clusters, 1)
-    print(target[i])
+    target[i] <- sample(x = clusters, size = 1, prob = p)
   }
   return(target)
 }
 
 # K-Means Assignment ------------------------------------------------------
 
+KMeans_initialization <- function(X, target, K, clusters){
+  
+}
 
 # Multinomial Assignment --------------------------------------------------
 
-
+Multinomial_initialization <- function(X, target, clusters){
+  
+}
 
 # Initialization Function -------------------------------------------------
 
@@ -41,16 +46,22 @@ initialization_fun <- function(method = "K-Means", target, X){
           "should be identified as NA")
   }
   
-  if (method == "K-Means") {
-    
+  # Identify proportion of observations in each pre-assigned cluster
+  p <- c()
+  n <- length(which(!is.na(target)))
+  for(i in clusters){
+    p[i] <- length(which(target == i)) / n
   }
-    
-  else if (method == "Random"){
-    
-  }
-    
-  else{
+  
+  if (method == "Random"){
+    target <- random_initialization(X, target, K, clusters, p)
+  }else if (method == "K-Means") {
+    target <- KMeans_initialization()
+  }else if (method == "Multinomial") {
+    target <- Multinomial_initialization()
+  }else{
     print("Incorrect initialization method specified, program will resort to
           default (K-Means)")
   }
+  return(target)
 }
